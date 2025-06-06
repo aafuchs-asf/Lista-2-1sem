c447e9c1e0217aa394c501750142729a90402802
aafuchs-asf <anafuchs.asf@gmail.com>
2025-06-05 23:35:26 -0400

    atividade 1

---
+++ atv1.py
@@ -0,0 +1,5 @@
+num = float(input('Por favor, digite um número de 1 a 10: '))
+
+while (num<1 or num>10):
+    print('Valor invalido!')
+    num = float(input('Por favor, digite um número de 1 a 10: '))
