1e7b3bc099c48f9a3e1e0da4035bd37d61ce8caa
aafuchs-asf <anafuchs.asf@gmail.com>
2025-06-06 21:45:05 -0400

    Atividade 10

---
+++ atvv10.py
@@ -0,0 +1,13 @@
+#Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles
+num1 = int(input('Digite o 1° número: '))
+num2 = int(input('Digite o 2° número: '))
+num = []
+soma = num1
+
+while soma < num2-1:
+    soma += 1
+    num.append(soma)
+
+print(num)
+
+
