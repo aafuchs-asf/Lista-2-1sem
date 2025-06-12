166690c19f5f5c8a9028d1b93a01212841fb0c6f
aafuchs-asf <anafuchs.asf@gmail.com>
2025-06-11 20:09:47 -0400

    Atividade 21

---
+++ atvv21.py
@@ -0,0 +1,18 @@
+num = int(input('Digite um número: '))
+y = True
+w = v = 0
+
+while y:
+    for i in range(num):
+        if i != 0 and i != 1 and v == 0:
+            w = num%i
+
+            if w == 0:
+                print(num,'não é um número primo!')
+                v = 1
+                y = False
+
+    if w != 0 and i != 1 and i != 0 and v == 0:
+        print(num,'é um número primo!')
+        y = False
+        w = 0
