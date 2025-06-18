9b7c5d9e256f2396c7b5a0ff4ed6e6c683373481
aafuchs-asf <anafuchs.asf@gmail.com>
2025-06-17 20:47:58 -0400

    Atividade 22

---
+++ atvv22.py
@@ -0,0 +1,29 @@
+#Altere o programa de cálculo dos números primos, informando, caso o número não seja primo, por quais
+#número ele é divisível.
+num = int(input('Digite um número: '))
+y = True
+w = v = 0
+div = []
+
+while y:
+    for i in range(num):
+        if i != 0 and i != 1:
+            w = num%i
+
+            if i != 0 and i != 1:
+                w = num%i
+
+                if w == 0:
+                    div.append(i)
+                    
+    if w != 0 and i != 1 and i != 0:
+        y = False
+        w = 0 
+
+if not div:
+    print(num,'é primo')
+
+else:
+    print(div)
+
+
