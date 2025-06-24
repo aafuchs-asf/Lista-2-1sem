71dd39f43afa696a21d24d45944f269a8956e29f
aafuchs-asf <anafuchs.asf@gmail.com>
2025-06-24 01:48:44 -0400

    Atividades

--- atvv40.py
+++ atvv40.py
@@ -1,9 +1,39 @@
-# Foi feita uma estatística em cinco cidades brasileiras para coletar dados sobre acidentes de trânsito. Foram
-# obtidos os seguintes dados:
-# a) Código da cidade;
-# b) Número de veículos de passeio (em 1999);
-# c) Número de acidentes de trânsito com vítimas (em 1999). Deseja-se saber:
-# d) Qual o maior e menor índice de acidentes de trânsito e a que cidade pertence;
-# e) Qual a média de veículos nas cinco cidades juntas;
-# f) Qual a média de acidentes de trânsito nas cidades com menos de 2.000 veículos de passeio.
+listCit = []
+cod = numV = numA = sV = sA = medV = medA = l = medAA = sAA= 0
+for i in range (5):
+    cod = int(input('Código da cidade: '))
+    numV = int(input('Número total de carros: '))
+    numA = int(input('Número total de acidentes: '))
 
+    listCit.append([cod, numV, numA])
+
+meA = listCit[0]
+mA = listCit[0]
+
+for p in listCit:
+    if p[2] > mA[2]:
+        mA = p
+    if p[2] < meA[2]:
+        meA = p
+
+for j in listCit:
+    sV += j[1]
+
+medV = sV/5
+
+for w in listCit:
+    sA += j[2]
+
+medA = sA/5
+
+for k in listCit:
+    if k[1] < 2000:
+        sAA += j[2]
+        l += 1
+
+medAA = sAA/l
+
+print(f'Menos acidentes: {meA}')
+print(f'Mais acidentes: {mA}')
+print(f'Média de veiculos: {medV}')
+print(f'Média de acidentes: {medA}')
